af12d5a64fe53d5593c6956a88f721d264506602
Daybreak2019 <li.wen@wsu.edu>
2021-04-01 23:17:45 -0700

    ast decode

--- PyComponent/PyInspect/AstVisit.py
+++ PyComponent/PyInspect/AstVisit.py
@@ -8,7 +8,7 @@ from copy import deepcopy, copy
 
 class ASTWalk(NodeVisitor):
     def __init__(self):
-        pass
+        self.FuncDef = {}
     
     def visit(self, node):
         """Visit a node."""
@@ -18,10 +18,34 @@ class ASTWalk(NodeVisitor):
         visitor = getattr(self, method, self.generic_visit)
         return visitor(node)
 
-    def visit_functiondef(self, node, expand=False):
-        pass
+    def _GetArgs (self, Stmt):
+        ArgList = []
+        Args = Stmt.args.args
+        for arg in Args:
+            if Stmt.name == "__init__" and arg.arg == "self":
+                continue
+            ArgList.append (arg.arg)
+        return ArgList
 
-    def visit_call(self, node, needtarget=True):
-        pass
+    def _ProcCall(self, Caller, Stmt):
+        print ("\tcall -->", Caller, ": ", ast.dump (Stmt))
+
+    def visit_functiondef(self, node):
+        FuncName = node.name
+        if FuncName[0:5] != "test_":
+            return
+        ArgList  = self._GetArgs (node)
+
+        Body = node.body
+        for Stmt in Body:
+            if isinstance (Stmt, Assign):
+                if isinstance (Stmt.value, Call):
+                    self._ProcCall (FuncName, Stmt.value) 
+            elif isinstance (Stmt, Call):
+                self._ProcCall (FuncName, Stmt.value)
+            else:
+                continue
+        return
+
 
     
